8b141e9c4b3e9eef13f34b8944fef8d7b0ab4276
unknown <apela@email.neit.edu>
2023-05-07 17:00:30 -0400

    goofy

---
+++ SE137/w6_class_ex/script.js
@@ -0,0 +1,35 @@
+var toggle = document.querySelector(`#toggle`)
+var tray = document.querySelector(`.tray`)
+
+var tabs = Array.from(document.querySelectorAll(`nav a`))
+var p = document.querySelector(`#editor p`)
+
+
+console.log(tabs)
+
+toggle.addEventListener(`click`, (e)=>{
+    e.preventDefault();
+    tray.classList.toggle(`closed`)
+})
+
+
+for(let i=0; i<tabs.length; i++)
+{
+    tabs[i].addEventListener(`click`, selectTab);
+}
+
+
+
+function selectTab(e)
+{
+    for(let i=0; i<tabs.length; i++)
+    {
+        tabs[i].style.backgroundColor =`#373737`;
+    }
+    
+    e.target.style.backgroundColor=`#161616`;
+    p.innerHTML =  `<a href=#>SE137</a> > 
+                    <a href=#>W6</a> >
+                    <a href=#>Homework Assignment</a> >
+                    <a href=#>Tab ${tabs.indexOf(e.target)+1}</a>`
+}
