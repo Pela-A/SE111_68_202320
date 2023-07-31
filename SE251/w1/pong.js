bc51c1091ae9ae5b2b5d6020c43105bfd9a21324
Pela <apela@email.neit.edu>
2023-07-31 12:27:40 -0400

    paddle array

--- SE251/w1/pong.js
+++ SE251/w1/pong.js
@@ -7,16 +7,19 @@ var fy = .85
 
 //Create player array and assign two Player objects to 0 and 1 index
 var player = []
+var pad = []
 for(let i=0; i<2; i++){
     player[i] = new Player();
     player[i].pad = new Box();
-    player[i].pad.w = 20
-    player[i].pad.h = 150
+    pad[i]= player[i].pad
+
+    pad[i].w = 20
+    pad[i].h = 150
 
 }
-player[0].pad.x = 0 + player[0].pad.w/2
-player[1].pad.x = 800 - player[1].pad.w/2
-player[1].pad.color = `blue`
+pad[0].x = 0 + pad[0].w/2
+pad[1].x = 800 - pad[1].w/2
+pad[1].color = `green`
 
 
 
@@ -30,10 +33,10 @@ ball.h = 20
 ball.vx=-2
 ball.vy=-2
 ball.color = `white`
-/*var player[1].pad = new Box();
-player[1].pad.w = 20
-player[1].pad.h = 150
-player[1].pad.x = 800 - player[1].pad.w/2*/
+/*var pad[1] = new Box();
+pad[1].w = 20
+pad[1].h = 150
+pad[1].x = 800 - pad[1].w/2*/
 
 
 function main()
@@ -42,63 +45,66 @@ function main()
     
     //if keyboard detects w key move up
     if(keys[`w`]){
-        player[0].pad.vy += -player[0].pad.force
+        pad[0].vy += -pad[0].force
     }
     //if keyboard detects s key move down
     if(keys[`s`]){
         //wall y position add velocity y
-        player[0].pad.vy += player[0].pad.force
+        pad[0].vy += pad[0].force
     }
 
     //if keyboard detects ArrowUp key move up
     if(keys[`ArrowUp`]){
-        player[1].pad.vy += -player[1].pad.force
+        pad[1].vy += -pad[1].force
     }
     //if keyboard detects ArrowDown key move down
     if(keys[`ArrowDown`]){
         //wall y position add velocity y
-        player[1].pad.vy += player[1].pad.force
+        pad[1].vy += pad[1].force
     }
 
     //multiply velocity by friction value to create terminal velocity feeling
-    player[0].pad.vy *= fy
-    player[1].pad.vy *= fy
+    pad[0].vy *= fy
+    pad[1].vy *= fy
 
     //move method called for all objects
-    player[0].pad.move();
-    player[1].pad.move();
+    pad[0].move();
+    pad[1].move();
     ball.move();
     
     //if wall y position less than 0+height of wall/2
-    if(player[0].pad.y < 0+player[0].pad.h/2)
+    if(pad[0].y < 0+pad[0].h/2)
     {
-        player[0].pad.y = 0+player[0].pad.h/2
+        pad[0].y = 0+pad[0].h/2
     }
 
     //if player one y position greater than height of canvas minus half the height of player 1 (position of wall is center of wall)
     //essentially if the y position is 750 or 50 the wall is not able to move
-    if(player[0].pad.y > c.height-player[0].pad.h/2)
+    if(pad[0].y > c.height-pad[0].h/2)
     {
-        player[0].pad.y = c.height-player[0].pad.h/2
+        pad[0].y = c.height-pad[0].h/2
     }
 
-    if(player[1].pad.y < 0+player[1].pad.h/2)
+    //if wall y position less than 0+height of wall/2
+    if(pad[1].y < 0+pad[1].h/2)
     {
-        player[1].pad.y = 0+player[1].pad.h/2
+        pad[1].y = 0+pad[1].h/2
     }
-    if(player[1].pad.y > c.height-player[1].pad.h/2)
+    //if player one y position greater than height of canvas minus half the height of player 1 (position of wall is center of wall)
+    //essentially if the y position is 750 or 50 the wall is not able to move
+    if(pad[1].y > c.height-pad[1].h/2)
     {
-        player[1].pad.y = c.height-player[1].pad.h/2
+        pad[1].y = c.height-pad[1].h/2
     }
 
 
     //ball collision
-    if(ball.collide(player[0].pad)){
-        ball.x = player[0].pad.x + player[0].pad.w/2 + ball.w/2
+    if(ball.collide(pad[0])){
+        ball.x = pad[0].x + pad[0].w/2 + ball.w/2
         ball.vx = -ball.vx
     }
-    if(ball.collide(player[1].pad)){
-        ball.x = player[1].pad.x - player[1].pad.w/2 - ball.w/2
+    if(ball.collide(pad[1])){
+        ball.x = pad[1].x - pad[1].w/2 - ball.w/2
         ball.vx = -ball.vx
     }
 
@@ -131,8 +137,8 @@ function main()
     }
 
     //draws ball and players
-    player[0].pad.draw()
-    player[1].pad.draw()
+    pad[0].draw()
+    pad[1].draw()
     ball.draw()
     
 
