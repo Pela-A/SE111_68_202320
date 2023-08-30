ea083e5700675b3fa933f0995ee16f14deba5263
Pela <apela@email.neit.edu>
2023-08-30 14:47:50 -0400

    w6 done

--- SE251/Simple Form Validation/Simple Form Validation/week2.js
+++ SE251/Simple Form Validation/Simple Form Validation/week2.js
@@ -2,65 +2,129 @@
 
 var inputs = document.querySelectorAll(`input`)
 
-console.log(inputs)
-var labels = ["first name", "last name","email" ,"confirm email","phone number"]
-
-console.log(labels.innerHTML)
-
-
-
 var submitBtn = document.querySelector(`#submitBtn`)
-
 var info = document.querySelector(`#info`)
-
-
-
-
-
-
+var fdback = document.querySelector(`#feedback`)
 
 submitBtn.addEventListener(`click`, (e)=>{
-    
-    
-    
-    for(let i=0; i < inputs.length - 1; i ++){
-        if(inputs[i].value !== ""){
-            inputs[i].parentElement.classList.remove(`error`)
-            inputs[i].nextElementSibling.innerHTML = ""
-            console.log("yo")
-        }
-        else{
-            inputs[i].parentElement.classList.add(`error`)
- 
-            inputs[i].nextElementSibling.innerHTML = `* Please enter a ${labels[i]}`
-        }   
-    
-    
+    var message= ""
+      
+    //check first name******************
+    var pattern1 = /[^A-Za-z-]+/
+    if(inputs[0].value !== "" && !pattern1.test(inputs[0].value) )
+    {
+        inputs[0].parentElement.classList.remove(`error`)
+        inputs[0].nextElementSibling.innerHTML = ""
     }
-    if(inputs[2].value !== inputs[3].value){
-        inputs[2].nextElementSibling.innerHTML += "Emails do not match!"
+    else if(inputs[0].value == ""){
+        message += `<p>Please enter a first name.</p>`
+        inputs[0].parentElement.classList.add(`error`)
+        inputs[0].nextElementSibling.innerHTML = `*`
+    }
+    else{
+        message+="First Name contains special characters or numbers"
+        inputs[0].parentElement.classList.add(`error`)
+        inputs[0].nextElementSibling.innerHTML = `*`
     }
 
+    //check last name*****************************
+    if(inputs[1].value !== "" && pattern1.test(inputs[1].value) )
+    {
+        inputs[1].parentElement.classList.remove(`error`)
+        inputs[1].nextElementSibling.innerHTML = ""
+    }
+    else if(inputs[1].value == ""){
+        message += `<p>Please enter a last name.</p>`
+        inputs[1].parentElement.classList.add(`error`)
+        inputs[1].nextElementSibling.innerHTML = `*`
+    }
+    else{
+        message+="Last Name contains special characters" 
+        inputs[1].parentElement.classList.add(`error`)
+        inputs[1].nextElementSibling.innerHTML = `*`
+    }
+
+    //check email**************************
+    pattern2 = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+
+    if(pattern2.test(inputs[2].value))
+    {
+        inputs[2].parentElement.classList.remove(`error`)
+        inputs[2].nextElementSibling.innerHTML = ""
+    }
+    else if(inputs[2].value == ""){
+        message += `<p>Please enter an email.</p>`
+        inputs[2].parentElement.classList.add(`error`)
+        inputs[2].nextElementSibling.innerHTML = `*`
+    }
+    else{
+        message+="Please enter a valid email" 
+        inputs[2].parentElement.classList.add(`error`)
+        inputs[2].nextElementSibling.innerHTML = `*`
+    } 
+    //check confirm email
+    if(pattern2.test(inputs[3].value))
+    {
+        inputs[3].parentElement.classList.remove(`error`)
+        inputs[3].nextElementSibling.innerHTML = ""
+    }
+    else if(inputs[3].value == ""){
+        message += `<p>Please enter a confirm email.</p>`
+        inputs[3].parentElement.classList.add(`error`)
+        inputs[3].nextElementSibling.innerHTML = `*`
+    }
+    else if(inputs[2].value !== inputs[3].value){
+        inputs[2].parentElement.classList.add(`error`)
+        inputs[3].parentElement.classList.add(`error`)
+        inputs[2].nextElementSibling.innerHTML = `*`
+        inputs[3].nextElementSibling.innerHTML = `*`
+        message += `<p>Email and confirm emial must match!</p>`
+    }
+    else{
+        message+="Please enter a valid confirm email" 
+        inputs[3].parentElement.classList.add(`error`)
+        inputs[3].nextElementSibling.innerHTML = `*`
+    } 
+
+    //check phone number
+    pattern3 = /[0-9]{10}/
+
+    if(pattern3.test(inputs[4].value)){
+        inputs[4].parentElement.classList.remove(`error`)
+        inputs[4].nextElementSibling.innerHTML = ""
+    }
+    else{
+        message+=`<p>Invalid phone number</p>`
+        inputs[4].parentElement.classList.add(`error`)
+        inputs[4].nextElementSibling.innerHTML = `*`
+    }
+
+    console.log(message)
+    if(message == ""){
+        fdback.innerHTML = message
+        var person = {
+            fname: inputs[0].value,
+            lname: inputs[1].value,
+            email: inputs[2].value,
+            phone: inputs[4].value
+           }
+        console.log(person)
+        submitBtn.parentElement.style.display = "none"
     
     
-})
-    /*
-    var person = {
-        fname: firstName.value,
-        lname: lastName.value,
-        email: email.value,
-        phone: phoneNum.value
-       }
-    console.log(person)
-    submitBtn.parentElement.style.display = "none"
+        var message = ""
+        message += person.fname + " " + person.lname
+        message += "<br>"
+        message += person.email
+        message += "<br>"
+        message += `${person.phone.substring(0,3)}-${person.phone.substring(3,6)}-${person.phone.substring(6,)}`
+        info.innerHTML = message
+        info.parentElement.style.display = "block"
 
+    }
+    else{
+        fdback.innerHTML = message
+    }
 
-    var message = ""
-    message += person.fname + " " + person.lname
-    message += "<br>"
-    message += person.email
-    message += "<br>"
-    message += person.phone
-    info.innerHTML = message
-    info.parentElement.style.display = "block"*/
-
+    console.log(fdback)
+})//end of submit button event listener
